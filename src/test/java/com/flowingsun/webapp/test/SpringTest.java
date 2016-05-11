ec6fb788d30f588442dc92c692b29e0c0ef8f6ab
flowingsun <yliu2oo1@hotmail.com>
2016-05-11 14:42:38 +0800

    fix spring config xml component scan problem

--- src/test/java/com/flowingsun/webapp/test/SpringTest.java
+++ src/test/java/com/flowingsun/webapp/test/SpringTest.java
@@ -3,15 +3,21 @@ package com.flowingsun.webapp.test;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.flowingsun.webapp.dao.*;
 import com.flowingsun.webapp.domain.*;
 
 import junit.framework.Assert;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+//@ContextConfiguration({"classpath:applicationContext.xml","classpath:spring.xml"})
 public class SpringTest {
 
 	@Autowired
@@ -39,6 +45,7 @@ public class SpringTest {
 	@Test
 	public void testSP() {
 		List<Integer> list = mealMenuDao.GetAvailableMealMenuIds();
+		System.out.print(list);
 		Assert.assertTrue(!list.isEmpty() && list.size() > 1);
 	}
 }
